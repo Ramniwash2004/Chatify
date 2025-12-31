a262fb60cd0b8695c3595f572e73ce21b15c643d
Ramniwash2004 <kramniwash68@gmail.com>
2025-12-31 19:39:49 +0530

    first commit

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const router=express.Router();
+
+router.get("/signup",(req,res)=>{
+    res.send("Hello from signup route");
+});
+
+router.get("/login",(req,res)=>{
+    res.send("Hello from login route");
+});
+
+router.get("/logout",(req,res)=>{
+    res.send("Hello from logout route");
+});
+
+
+export default router;
